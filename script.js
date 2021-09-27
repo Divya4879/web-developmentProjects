b94085644ddb1abacaf0c7871bd8c71f684b7b59
Divya4879 <86043076+Divya4879@users.noreply.github.com>
2021-09-27 15:39:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,8 +58,7 @@ WARNING!!! WARNING!!!
     // byeSpeaker.xxxx
  // } else {
     // helloSpeaker.xxxx
- // }
-//}
+
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
